fd139832ee7b803015e8d7839a1956f151501b1b
Raja G <38761990+Raja7022@users.noreply.github.com>
2020-03-10 18:54:15 +0530

    Add files via upload
    python program to generate mad libs generator

---
+++ mad_libs_generator.py
@@ -0,0 +1,7 @@
+def mad_libs_generator():
+    la=input("enter the noun " )
+    mad=input("enter adjective " )   
+    lib=input("enter noun " ) 
+    print(" i am big fan of {} .she likes {}.my name is {}".format(la,mad,lib))    
+
+mad_libs_generator()     
